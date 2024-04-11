6e8663c6041f2d554c0daef3bae05b1a99a72efa
Nora0000 <mengjingliu0@gmail.com>
2024-04-11 01:05:49 -0400

    split to train, validation, test. save test with .npy

--- ReprogramLLM/data_loader.py
+++ ReprogramLLM/data_loader.py
@@ -12,17 +12,22 @@ def load_dataFile(path_list, train_size=0.8):
 	y_train = []
 	X_test = []
 	y_test = []
+	X_val = []
+	y_val = []
 	for path in path_list:
 		# path1 = "/home/mengjingliu/Vid2Doppler/data/2023_07_19/HAR6"
 		x1 = np.load(os.path.join(path, "X_4.npy"))[:, np.newaxis, :, :]
 		y1 = np.load(os.path.join(path, "Y_4.npy")) - 1
 		x1_train, x1_test, y1_train, y1_test = train_test_split(x1, y1, train_size=train_size, random_state=42)
-		
+		x1_train, x1_val, y1_train, y1_val = train_test_split(x1_train, y1_train, train_size=train_size, random_state=42)
+
 		if len(X_train) == 0:
 			X_train = x1_train
 			y_train = y1_train
 			X_test = x1_test
 			y_test = y1_test
+			X_val = x1_val
+			y_val = y1_val
 		else:
 			X_train = np.vstack((x1_train, X_train))
 			y_train = np.concatenate((y1_train, y_train))
@@ -30,7 +35,21 @@ def load_dataFile(path_list, train_size=0.8):
 			X_test = np.vstack([x1_test, X_test])
 			y_test = np.concatenate([y1_test, y_test])
 
-	return torch.from_numpy(X_train).float(), torch.from_numpy(y_train).long(), torch.from_numpy(X_test).float(), torch.from_numpy(X_test).float()
+			X_val = np.vstack([x1_val, X_val])
+			y_val = np.concatenate([y1_val, y_val])
+	
+	X = np.vstack([X_train, X_test, X_val])
+	x_mean = np.mean(X)
+	x_std = np.std(X)
+	X_train = (X_train - x_mean) / x_std
+	X_test = (X_test - x_mean) / x_std
+	X_val = (X_val - x_mean) / x_std
+
+
+	np.save("X_test.npy", X_test)
+	np.save("y_test.npy", y_test)
+
+	return torch.from_numpy(X_train).float(), torch.from_numpy(y_train).long(), torch.from_numpy(X_val).float(), torch.from_numpy(y_val).long()
 
 def resize_tensor(train_data, test_data, size=(224, 224)):
 	resized_train_data = F.interpolate(train_data, size=size,mode='bilinear', align_corners=False)
@@ -50,10 +69,13 @@ def data_loader(train_dataset, test_dataset, batch_size=16):
 	return train_loader, test_loader
 
 
-def wrapper_dataLoader(path_list, train_size=0.8, batch_size=16, if_resize=False):
+def wrapper_dataLoader(path_list, train_size=0.8, batch_size=16, if_resize=False, if_replicate_channels=False):
 	train_data, train_label, test_data, test_label = load_dataFile(path_list, train_size=train_size)
 	if if_resize:
 		train_data, test_data = resize_tensor(train_data, test_data)
+	if if_replicate_channels:
+		train_data = train_data.repeat(1, 3, 1, 1)
+		test_data = test_data.repeat(1, 3, 1, 1)
 	train_set, test_set = convert_to_dataset(train_data, train_label, test_data, test_label)
 	train_loader, test_loader = data_loader(train_set, test_set, batch_size=batch_size)
 	return train_loader, test_loader
@@ -65,10 +87,7 @@ if __name__ == "__main__":
 				"/home/mengjingliu/Vid2Doppler/data/2023_07_19/HAR4",
 				"/home/mengjingliu/Vid2Doppler/data/2023_11_17/HAR3",
 				"/home/mengjingliu/Vid2Doppler/data/2023_07_19/HAR2"]
-	train_data, train_label, test_data, test_label = load_dataFile(path_list, train_size=0.8)
-	train_data, test_data = resize_tensor(train_data, test_data)
-	train_set, test_set = convert_to_dataset(train_data, train_label, test_data, test_label)
-	train_loader, test_loader = data_loader(train_set, test_set, batch_size=16)
+	wrapper_dataLoader(path_list, batch_size=16, if_resize=True, if_replicate_channels=True)
 
 
 
