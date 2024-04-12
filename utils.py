eddcc2e7f4f2dcb238f3515ceef15d7080da15e7
Nora0000 <mengjingliu0@gmail.com>
2024-04-11 20:18:20 -0400

    finetune last layer

--- utils.py
+++ utils.py
@@ -6,14 +6,14 @@ import numpy as np
 
 def plot_loss(path, filename, training_loss, test_loss, test_accuracy, test_interval=10):
 	plt.plot(training_loss, label='Training Loss')
-	plt.plot(np.arange(1, len(test_loss)+1, 1) * test_interval, test_loss, label='Test Loss')
-	plt.plot(np.arange(1, len(test_accuracy)+1, 1) * test_interval, test_accuracy, label='Test Accuracy')
+	plt.plot(np.arange(1, len(test_loss)+1, 1) * test_interval, test_loss, label='Val Loss')
+	plt.plot(np.arange(1, len(test_accuracy)+1, 1) * test_interval, test_accuracy, label='Val Accuracy')
 	plt.xlabel('Epochs')
 	plt.ylabel('Loss & Accuracy')
 	
 	plt.legend()
 	plt.grid()
-	plt.title("loss & test accuracy")
+	plt.title("loss & accuracy")
 	plt.savefig(os.path.join(path, filename))
 	plt.cla()
 	# plt.show()
