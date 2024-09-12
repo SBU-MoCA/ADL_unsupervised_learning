2458b5bada4ca7c2022623e7b6d8e3c74a838f4e
Nora0000 <mengjingliu0@gmail.com>
2024-09-12 19:10:02 -0400

    set timezone properly

--- helper.py
+++ helper.py
@@ -5,6 +5,20 @@ import numpy as np
 import datetime
 from datetime import timedelta, timezone
 # from scipy.signal import butter, lfilter
+import imutils
+from matplotlib import cm
+import cv2
+
+
+def color_scale(img, norm, text=None):
+	if len(img.shape) == 2:
+		img = cm.magma(norm(img), bytes=True)
+	img = imutils.resize(img, height=300)
+	if img.shape[2] == 4:
+		img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+	if text is not None:
+		img = cv2.putText(img, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+	return img
 
 
 def butter_lowpass(cutoff, fs, order=5):
@@ -41,21 +55,22 @@ def first_peak(std_dis):
     return left, right
 
 
-def datetime_from_str(str):
+def datetime_from_str(str, tzinfo=None):
+    # timezone: default is system local time
     try:
-        r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%d %H:%M:%S.%f')
+        r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%d %H:%M:%S.%f').replace(tzinfo=tzinfo)
     except Exception as e:
         try:
-            r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%d %H:%M:%S')
+            r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%d %H:%M:%S').replace(tzinfo=tzinfo)
         except Exception as e:
             try:
-                r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%dT%H:%M:%S.%fZ')
+                r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=tzinfo)
             except Exception as e:
                 try:
-                    r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%dT%H:%M:%SZ')
+                    r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=tzinfo)
                 except Exception as e:
                     try:
-                        r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%dT%H:%M:%S.%fZZ')
+                        r = datetime.datetime.strptime(str.strip('\n'), '%Y-%m-%dT%H:%M:%S.%fZZ').replace(tzinfo=tzinfo)
                     except Exception as e:
                         print("exception: ", str)
                         return ""
@@ -63,16 +78,20 @@ def datetime_from_str(str):
 
 
 def load_txt_to_datetime(path, filename):
+    """
+    This is to load timestamp of UWB data exported from InfluxDB. the timezone is UTC time.
+    """
     # load txt file which has a str of datetime in each line. convert str to datetime, return the list of all lines.
     with open(os.path.join(path, filename), 'r') as f:
         lines = f.readlines()
-        return [datetime_from_str(line) for line in lines]
+        return [datetime_from_str(line, tzinfo=timezone.utc) for line in lines]     # indicate the timezone is UTC time
 
 
-def load_segment_file_to_datetime(path, filename, if_local=True):
+def load_segment_file_to_datetime(path, filename, if_local=True, start_seg=0, stop_seg=None):
     """
     load App format segment file from android app, return datetime list [start, stop].
     APP segment file format: "activity name - start: datetime,stop: datetime"
+    In 2023, it is local time. In 2024, it is utc time.
     
     Args:
         path:
@@ -86,19 +105,27 @@ def load_segment_file_to_datetime(path, filename, if_local=True):
     acts = []
     with open(os.path.join(path, filename), 'r') as f:
         lines = f.readlines()
-        
+         
+        cnt = 0
         for line in lines:
+            if cnt < start_seg:
+                cnt += 1
+                continue
+            if cnt > stop_seg:
+                break
+            cnt += 1
             act = line.strip('\n').split(' - ')[0]
             acts.append(act)
-            ss = line.strip('\n').split(' - ')[1].split(',')
-            dt.append(datetime_from_str(ss[0][7:]).astimezone(timezone.utc), datetime_from_str(ss[1][6:]).astimezone(timezone.utc))
-            # if if_local:
-            #     dt = [[datetime_from_str(line.split(',')[0][6:]).astimezone(timezone.utc),
-            #            datetime_from_str(line.strip('\n').split(',')[1][5:]).astimezone(timezone.utc)]
-            #           for line in lines]
-            # else:
-            #     dt = [[datetime_from_str(line.split(',')[0][6:]), datetime_from_str(line.strip('\n').split(',')[1][5:])]
-            #           for line in lines]
+            try:
+                ss = line.strip('\n').split(' - ')[1].split(',')
+            except Exception as e:
+                print(line)
+            if datetime_from_str(ss[0][6:]).year < 2024:
+                dt.append([datetime_from_str(ss[0][6:]).astimezone(timezone.utc), datetime_from_str(ss[1][5:]).astimezone(timezone.utc)])   # convert timezone to UTC time. default is the local time of the OS
+            else:
+                dt.append([datetime_from_str(ss[0][6:], tzinfo=timezone.utc), datetime_from_str(ss[1][5:], tzinfo=timezone.utc)])   # convert timezone to UTC time. default is the local time of the OS
+
+            # print(ss[0][6:], ss[1][5:], "\n", datetime_from_str(ss[0][6:]), datetime_from_str(ss[1][5:]), "\n", datetime_from_str(ss[0][6:]).astimezone(timezone.utc), datetime_from_str(ss[1][5:]).astimezone(timezone.utc), "\n\n")
     return dt, acts
 
 
@@ -124,6 +151,7 @@ return the frame indices of each segment in UWB date.
     """
     # path = "/home/mengjingliu/ADL_data/I2HSeJ_ADL_1"
     # filename = "b8-27-eb-85-a7-83.csv"
+
     node_id = filename.split('.')[0]
     ts_uwb_file = os.path.join(path, node_id + '_timestamp.txt')
     
@@ -131,9 +159,9 @@ return the frame indices of each segment in UWB date.
     # filename_seg = "2023-07-03-16-50-45_I2HSeJ_ADL_1.txt"
     
     dt = load_txt_to_datetime(path, ts_uwb_file)    # load timestamp of each data frame
-    seg, acts = load_segment_file_to_datetime(path_seg, filename_seg)     # load segmentation from android app
-    
-    i = start_seg
+    seg, acts = load_segment_file_to_datetime(path_seg, filename_seg, start_seg=start_seg, stop_seg=stop_seg)     # load segmentation from android app
+
+    i = 0
     start, stop = seg[i][0], seg[i][1]
     start_ind = 0
     stop_ind = len(dt)
